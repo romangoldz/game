abde1ee0b1d84fc5d25bad505d8ea2552438160c
romangoldz <roman.goldzitskiy.2011@gmail.com>
2026-08-13 22:01:24 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import toga
+from toga.style import Pack
+from toga.style.pack import COLUMN, CENTER
+
+class MyApp(toga.App):
+    def startup(self):
+        main_box = toga.Box(style=Pack(direction=COLUMN, alignment=CENTER))
+        label = toga.Label("Hello World!", style=Pack(padding=10))
+        main_box.add(label)
+        self.main_window = toga.MainWindow(title="My App")
+        self.main_window.content = main_box
+        self.main_window.show()
+
+def main():
+    return MyApp()
